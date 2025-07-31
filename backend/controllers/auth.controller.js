f81d7b880830d5c31ff11f0bb2924a8710fc42df
Ashfaque-Ahmed47 <ishfaque7878@gmail.com>
2025-07-31 23:19:20 +0500

    initial commits

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async  (req, res) =>{
+    res.send("I am at the signup page");
+}
+
+export const login = async  (req, res) =>{
+    res.send("I am at login page");
+}
+
+export const logout = async  (req, res) =>{
+    res.send("I am at the logout page");
+}
