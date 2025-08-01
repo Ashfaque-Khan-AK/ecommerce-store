7476fa27fb9014defd81976eb65e914121dd948a
Ashfaque-Ahmed47 <ishfaque7878@gmail.com>
2025-08-01 20:23:03 +0500

    added the redis.js and update the auth.controller and other fils

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,32 @@
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId) => {
+    const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "15m",
+    });
+    const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: "7d",
+    });
+    
+    return { accessToken, refreshToken};
+}
+
 export const signup = async  (req, res) =>{
-    res.send("I am at the signup page");
+    const { name, email, password } = req.body;
+    try {
+        const userExists = await User.findOne({ email });
+        if(userExists) return res.status(400).json({message: "Email already exist"});
+        
+        const user = await User.create({name, email, password});
+
+
+        const { accessToken, refreshToken} = generateToken(user._id);
+
+        res.status(201).json({user, message: "User created successfully!"})
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 }
 
 export const login = async  (req, res) =>{
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
@@ -8,6 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 
