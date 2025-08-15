98deb2fb5e89118e456f8e597ec1f686ecb24bdb
Ashfaque-Ahmed47 <ishfaque7878@gmail.com>
2025-08-15 23:09:33 +0500

    Update the logout function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -64,12 +64,18 @@ export const login = async  (req, res) =>{
 }
 
 export const logout = async  (req, res) =>{
-    res.send("I am at the logout page");
+    
     try {
         const refreshToken = req.cookies.refreshToken;
-        const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
-        await redis.del(`refresh_token:${decoded.userId}`)
-    } catch (error) {
+        if (refreshToken) {
+            const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+            await redis.del(`refresh_token:${decoded.userId}`) 
+        }
         
+        res.clearCookie("accessToken");
+        res.clearCookie("refreshToken");
+        res.json({ message: "Logged out successfully"});
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
     }
-}
+};
