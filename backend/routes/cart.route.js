c70dc1ac9d03b163ac6d4cc58db7fa308e994e20
Ashfaque-Ahmed47 <ishfaque7878@gmail.com>
2025-08-23 21:44:22 +0500

    added cart controllers and cart routes

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,6 +1,16 @@
 import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { 
+    addToCart, 
+    removeAllFromCart, 
+    updateQuantity
+ } from "../middleware/cart.controller.js";
 
 const router = express.Router();
+
+router.post("/", protectRoute, addToCart);
+router.delete("/", protectRoute, removeAllFromCart);
+router.put("/:id", protectRoute, updateQuantity);
 
 
 export default router; 
